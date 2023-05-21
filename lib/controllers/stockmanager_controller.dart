4e4d75efd441f3befe10bf853264c6e302e16b5d
gruto <gruto@naver.com>
2023-05-22 08:36:25 +0900

    first commit

--- lib/controllers/stockmanager_controller.dart
+++ lib/controllers/stockmanager_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class StockmanagerController extends GetxController {
@@ -6,7 +7,40 @@ class StockmanagerController extends GetxController {
   final dropdownValue = '배송선택'.obs;
   final List<String> deliveryList = ['배송선택', '유료배송', '무료배송'];
 
+  final isClick = false.obs;
+
+  final costPrice = ''.obs;
+  final commissionRate = ''.obs;
+  final earningRate = ''.obs;
+  final sellingPrice = ''.obs;
+  final deliveryCharge = ''.obs;
+
+  final commission = ''.obs;
+  final earning = ''.obs;
+
   void setSelected(String value) {
     dropdownValue.value = value;
+  }
+
+  Widget input_text(String title, String formValue) {
+    return Row(
+      children: [
+        Container(
+          alignment: Alignment.centerRight,
+          width: 60,
+          child: Text(title),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+            child: TextFormField(
+          onChanged: (val) {
+            formValue = val;
+          },
+          validator: (val) {
+            return null;
+          },
+        )),
+      ],
+    );
   }
 }
--- lib/views/widgets/components/goods_price_calculator.dart
+++ lib/views/widgets/components/goods_price_calculator.dart
@@ -1,8 +1,9 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:stockmanager/controllers/stockmanager_controller.dart';
+import 'package:stockmanager/views/screen/sale_home.dart';
 
 // class GoodsPriceCalculator {
 //   @override
@@ -69,11 +70,9 @@ import 'package:stockmanager/controllers/stockmanager_controller.dart';
 
 class GoodsPriceCalculator {
   final _formkey = GlobalKey<FormState>();
-  String costPrice = '';
-  String commissionRate = '';
-  String earningRate = '';
-  String sellingPrice = '';
-  String deliveryCharge = '';
+
+  var numberComma = NumberFormat('###,###,###');
+
   @override
   GoodsPriceCalculator() {
     Get.dialog(
@@ -81,19 +80,80 @@ class GoodsPriceCalculator {
         title: Text('상품 판매가 계산기 V1.0'),
         content: Form(
           key: _formkey,
-          child: Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                input_text('원가',costPrice),
-                input_text('수수료율', commissionRate),
-                input_text('수익률', earningRate),
-                Row(
-                  children: [
-                    Text('배송방법'),
-                    SizedBox(width: 15),
-                    Obx(
-                      () => DropdownButton<String>(
+          child: Obx(
+            () => Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //원가
+                  Row(
+                    children: [
+                      Container(
+                        alignment: Alignment.centerRight,
+                        width: 60,
+                        child: Text('원가'),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                          child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                        ],
+                        onChanged: (val) {
+                          StockmanagerController.to.costPrice.value = val;
+                        },
+                      )),
+                    ],
+                  ),
+                  //수수료율
+                  Row(
+                    children: [
+                      Container(
+                        alignment: Alignment.centerRight,
+                        width: 60,
+                        child: Text('수수료율'),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                          child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                        ],
+                        onChanged: (val) {
+                          StockmanagerController.to.commissionRate.value = val;
+                        },
+                      )),
+                    ],
+                  ),
+                  //수익률
+                  Row(
+                    children: [
+                      Container(
+                        alignment: Alignment.centerRight,
+                        width: 60,
+                        child: Text('수익률'),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                          child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                        ],
+                        onChanged: (val) {
+                          StockmanagerController.to.earningRate.value = val;
+                        },
+                      )),
+                    ],
+                  ),
+                  //배송방법 TextFormField
+                  Row(
+                    children: [
+                      Text('배송방법'),
+                      SizedBox(width: 15),
+                      DropdownButton<String>(
                         value: StockmanagerController.to.dropdownValue.value,
                         items: StockmanagerController.to.deliveryList
                             .map<DropdownMenuItem<String>>((String item) {
@@ -106,28 +166,79 @@ class GoodsPriceCalculator {
                           StockmanagerController.to.setSelected(val!);
                         },
                       ),
+                    ],
+                  ),
+                  //무료배송이면 배송비 입력란이 나오게
+                  if (StockmanagerController.to.dropdownValue == '무료배송')
+                    //배송비
+                    Row(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerRight,
+                          width: 60,
+                          child: Text('배송비'),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                            child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                          ],
+                          onChanged: (val) {
+                            StockmanagerController.to.deliveryCharge.value =
+                                val;
+                          },
+                        )),
+                      ],
                     ),
-                  ],
-                ),
-                //무료배송이면 배송비 입력란이 나오게
-                // StockmanagerController.to.dropdownValue == '무료배송' ? input_text('배송비') : Container(),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: (){
-
-                  },
-                  child: Text('판매가 계산'),
-                ),
-                SizedBox(height: 15),
-                Row(
-                  children: [
-                    Text('상품 판매가'),
-                    SizedBox(width: 15
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        _formkey.currentState?.save();
+                        StockmanagerController.to.isClick.value = true;
+                      }
+                      sellingPriceCalculate(
+                              StockmanagerController.to.costPrice.value,
+                              StockmanagerController.to.earningRate.value,
+                              StockmanagerController.to.commissionRate.value,
+                              StockmanagerController.to.deliveryCharge.value)
+                          .toString();
+                      commissionCalaulate(
+                          StockmanagerController.to.commissionRate.value);
+                      earningCalaulate(
+                          StockmanagerController.to.earningRate.value);
+                    },
+                    child: Text('판매가 계산'),
+                  ),
+                  SizedBox(height: 15),
+                  if (StockmanagerController.to.isClick.value)
+                    Column(
+                      children: [
+                        calculateResult(
+                            '상품판매가',
+                            numberComma.format(int.parse(
+                                StockmanagerController.to.sellingPrice.value))),
+                        calculateResult(
+                            '수수료',
+                            numberComma.format(int.parse(
+                                StockmanagerController.to.commission.value))),
+                        calculateResult(
+                            '수익',
+                            numberComma.format(int.parse(
+                                StockmanagerController.to.earning.value))),
+                      ],
                     ),
-                    Text('')
-                  ],
-                )
-              ],
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: Text('닫기'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -135,32 +246,64 @@ class GoodsPriceCalculator {
     );
   }
 
-  Widget input_text(String title, String formValue) {
+  //판매가 계산기
+  sellingPriceCalculate(String costPrice, String earningRate,
+      String commissionRate, String deliveryCharge) {
+    if (StockmanagerController.to.dropdownValue.value == '유료배송') {
+      StockmanagerController.to.sellingPrice.value = ((((int.parse(costPrice) /
+                          (1 -
+                              int.parse(earningRate) / 100 -
+                              int.parse(commissionRate) / 100)) /
+                      10)
+                  .round()) *
+              10)
+          .toString();
+    } else if (StockmanagerController.to.dropdownValue.value == '무료배송') {
+      StockmanagerController.to.sellingPrice.value =
+          (((((int.parse(costPrice) + int.parse(deliveryCharge)) /
+                              (1 -
+                                  int.parse(earningRate) / 100 -
+                                  int.parse(commissionRate) / 100)) /
+                          10)
+                      .round()) *
+                  10)
+              .toString();
+    }
+    return StockmanagerController.to.sellingPrice.value;
+  }
+
+  //수수료 계산기
+  commissionCalaulate(String commissionRate) {
+    StockmanagerController.to.commission.value =
+        ((((int.parse(StockmanagerController.to.sellingPrice.value) *
+                            (int.parse(commissionRate) / 100)) /
+                        10)
+                    .round()) *
+                10)
+            .toString();
+  }
+
+  //수익 계산기
+  earningCalaulate(String earningRate) {
+    StockmanagerController.to.earning.value =
+        ((((int.parse(StockmanagerController.to.sellingPrice.value) *
+                            (int.parse(earningRate) / 100)) /
+                        10)
+                    .round()) *
+                10)
+            .toString();
+  }
+
+  Widget calculateResult(String title, String result) {
     return Row(
       children: [
         Text(title),
         SizedBox(width: 15),
-        Expanded(
-            child: TextFormField(
-          onChanged: (val) {
-            formValue = val;
-          },
-        )),
+        Text('$result 원'),
       ],
     );
   }
-
-  sellingPriceCalculate(){
-    if(StockmanagerController.to.dropdownValue.value == '유료배송') {
-      sellingPrice = ((((int.parse(costPrice) / (1 - int.parse(earningRate) - int.parse(commissionRate))) /10).round())*10).toString();
-  } else if(StockmanagerController.to.dropdownValue.value == '무료배송') {
-      sellingPrice = (((((int.parse(costPrice)+int.parse(deliveryCharge)) / (1 - int.parse(earningRate) - int.parse(commissionRate))) /10).round())*10).toString();
-    }
-
-  }
 }
-
-
 
 // class DeliveryDropdownButton extends StatefulWidget {
 //   const DeliveryDropdownButton({Key? key}) : super(key: key);
