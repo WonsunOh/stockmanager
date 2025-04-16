e531ac9fca89b9d287e8fd78cbe9a4f3c693de20
gruto <gruto@naver.com>
2025-04-17 05:09:37 +0900

    sdcond commit 2nd version

--- lib/views/screen/my_home.dart
+++ lib/views/screen/my_home.dart
@@ -23,7 +23,7 @@ class _MyHomeState extends State<MyHome> {
         centerTitle: true,
         automaticallyImplyLeading: false,
         title: const Text(
-          '코스트고 재고관리 1.1',
+          '코스트고 재고관리 1.2',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
